1bcb9e86288d8ae2fb2f358588d0389cdc8c4f82
aftiyah <abd2010ly@gmail.com>
2020-04-23 18:48:51 +0200

    Question actions and reducer update

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,18 @@
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ADD_ANSWER_TO_QUESTION = 'ADD_ANSWER_TO_QUESTION'
 
 export function receiveQuestions(questions) {
     return {
         type: RECEIVE_QUESTIONS,
         questions
+    }
+}
+
+export function addAnswerToQuestion(authUser, qid, answer) {
+    return { 
+        type: ADD_ANSWER_TO_QUESTION,
+        authUser,
+        qid,
+        answer
     }
 }
--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,7 @@
-import { RECEIVE_QUESTIONS } from '../actions/questions'
+import { 
+    RECEIVE_QUESTIONS,
+    ADD_ANSWER_TO_QUESTION
+ } from '../actions/questions'
 
 export default function questions(state={}, action) {
 
@@ -8,6 +11,18 @@ export default function questions(state={}, action) {
                 ...state,
                 ...action.questions
             }
+            case ADD_ANSWER_TO_QUESTION:
+                const { authUser, qid, answer } = action
+                return {
+                    ...state,
+                    [qid]: {
+                        ...state[qid],
+                        [answer]: {
+                            ...state[qid][answer],
+                            votes: state[qid][answer].votes.concat(authUser)
+                        }
+                    }
+                }
         default :
             return state
     }
