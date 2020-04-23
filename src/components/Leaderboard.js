23a4c7ac4f865f027769320a57c1dc181a95240e
aftiyah <abd2010ly@gmail.com>
2020-04-23 20:35:48 +0200

    Complete Leaderboard view

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -24,7 +24,48 @@ export class Leaderboard extends Component {
 
         return (
             <Fragment>
-
+                                { leaderboardData.map((user, index) => (
+                    <Segment.Group key={user.id} >
+                        <Label corner='left' color={colors[index]} />
+                        <Grid divided padded>
+                            <Grid.Row>
+                                <Grid.Column width={4} verticalAlign='middle'>
+                                    <Image src={user.avatarURL} />
+                                </Grid.Column>
+                                <Grid.Column width={8}>
+                                    <Header as='h3' textAlign='left'>
+                                        { user.name }
+                                    </Header>
+                                    <Grid>
+                                        <Grid.Column width={12}>
+                                            Asked:
+                                        </Grid.Column>
+                                        <Grid.Column width={4}>
+                                            { user.questionCount }
+                                        </Grid.Column>
+                                    </Grid>
+                                    <Divider />
+                                    <Grid>
+                                        <Grid.Column width={12}>
+                                            Answered:
+                                        </Grid.Column>
+                                        <Grid.Column width={4}>
+                                            { user.answerCount }
+                                        </Grid.Column>
+                                    </Grid>
+                                </Grid.Column>
+                                <Grid.Column width={4} textAlign="center">
+                                    <Header as='h5' block attached='top' content="Total" />
+                                    <Segment>
+                                        <Label color='green' size='big'>
+                                            { user.questionCount + user.answerCount }
+                                        </Label>
+                                    </Segment>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Segment.Group>
+                ))}
             </Fragment>
         )
     }
@@ -36,8 +77,8 @@ function mapStateToProps({ users }) {
         name: user.name,
         avatarURL: user.avatarURL,
         answerCount: Object.values(user.answers).length,
-        questionCount: user.question.length,
-        total: Object.values(user.answers).length + user.question.length,
+        questionCount: user.questions.length,
+        total: Object.values(user.answers).length + user.questions.length,
     }))
     .sort((a, b) => a.total - b.total)
     .reverse()
