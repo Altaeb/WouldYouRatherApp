c59960d39badf449179844e25d868c5f15c303ee
aftiyah <abd2010ly@gmail.com>
2020-04-23 14:40:43 +0200

    Questions actions updated

--- src/actions/authUser.js
+++ src/actions/authUser.js
@@ -1,8 +1,39 @@
-export const SET_AUTH_USER = 'SET_AUTH_USER'
+import { getUser } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
-export function setAuthUser(id) {
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
+
+export function receiveLogin(user) {
     return {
-        type: SET_AUTH_USER,
-        id 
+        type: LOGIN_SUCCESS,
+        authed: true,
+        loggedInUser: user
+    }
+}
+
+export function receiveLogout() {
+    return {
+        type: LOGOUT_SUCCESS,
+        authed: null,
+        loggedInUser: null
+    }
+}
+
+export function handleLogin(id) {
+    return dispatch => {
+        dispatch(showLoading())
+        getUser(id).then(user => {
+            dispatch(receiveLogin(user))
+            dispatch(hideLoading())
+        })
+    }
+}
+
+export function handleLogout() {
+    return dispatch => {
+        dispatch(showLoading())
+        dispatch(receiveLogout())
+        dispatch(hideLoading())    
     }
 }
