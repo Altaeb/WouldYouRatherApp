e9dc1d71bde33feb05cd7504aae210e13934cba8
aftiyah <abd2010ly@gmail.com>
2020-04-23 14:36:46 +0200

    Add loading-bar & API upgrade

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { setAuthUser } from '../actions/authUser'
+
+export class Login extends Component {
+
+
+    render () {
+        <div>
+            <h1>Welcome to Would You Rather App!</h1>
+            <h3>Please sign in to continue</h3>
+        </div>
+    }
+
+}
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,27 @@
 import {
     _getUsers,
+    _getUser,
     _getQuestions,
     _saveQuestion,
     _saveQuestionAnswer
 } from './_DATA'
 
-export function getInitialData() {
-    return Promise.all([_getUsers(), _getQuestions()])
-                .then(([users, questions]) => ({
-                    users,
-                    questions
-                }))
+export function saveQuestion (info) {
+    return _saveQuestion(info);
 }
 
-export function saveQuestion(question) {
-    return _saveQuestion(question)
+
+
+
+
+export function getUser (id) {
+    return _getUser(id);
 }
 
-export function saveQuestionAnswer(answer) {
-    return _saveQuestionAnswer(answer)
+export function getUsers() {
+    return _getUsers();
+}
+
+export function getQuestions() {
+    return _getQuestions();
 }
