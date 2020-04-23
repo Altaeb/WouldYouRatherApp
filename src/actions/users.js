4cf3c9bcdc379b33a4b2a1dfb5a31f25136cc67e
aftiyah <abd2010ly@gmail.com>
2020-04-23 14:44:14 +0200

    Users actions updated

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,39 @@
-export const RECEIVE_USERS = 'RECEIVE_USERS'
+import { getUsers } from "../utils/api";
+import { showLoading, hideLoading } from 'react-redux-loading';
+
+export const RECEIVE_USERS = 'RECEIVE_USERS';
+export const ADD_USER_QUESTION_ANSWER = 'ADD_USER_QUESTION_ANSWER';
+export const ADD_USER_QUESTION = 'ADD_USER_QUESTION';
 
 export function receiveUsers(users) {
     return {
         type: RECEIVE_USERS,
         users
+    }
+}
+
+export function addUserQuestion(question) {
+    return {
+        type: ADD_USER_QUESTION,
+        question
+    }
+}
+
+export function addUserQuestionAnswer(authedUser, questionID, answer) {
+    return {
+        type: ADD_USER_QUESTION_ANSWER,
+        authedUser,
+        questionID,
+        answer
+    }
+}
+
+export function handleGetUsers() {
+    return dispatch => {
+        dispatch(showLoading())
+        return getUsers().then(user => {
+            dispatch(receiveUsers(user))
+            dispatch(hideLoading())
+        })
     }
 } 
