3f8badf74b2298b9ca24f5fce35875d9d3aff0ed
aftiyah <abd2010ly@gmail.com>
2020-04-23 13:18:18 +0200

    Add action creators

---
+++ src/actions/hared.js
@@ -0,0 +1,14 @@
+import { getInitialData } from '../utils/api' 
+import { receiveQuestions } from './questions'
+import { receiveUsers } from './users'
+
+export function handleInitialData() {
+    return dispatch => {
+        return getInitialData().then(
+            ({users, questions}) => {
+                dispatch(receiveUsers(users))
+                dispatch(receiveQuestions(questions))
+            }
+        )
+    }
+}
